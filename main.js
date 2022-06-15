df3fef1e35b5b6fd71cfbf24fb5fe46056b4efb5
karelitz <karelitz>
2022-06-15 17:57:21 -0300

    entregado

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // palabras para el juego, agregar las que quieran siempre de 5 letras
 // es un array de strings (dict por dictionary)
 let dict = [
+  'ivank',
+  'lesta',
   'gatos',
   'papas',
   'fuego',
@@ -23,3 +25,38 @@ let dict = [
   'genio',
   'pibes'
 ];
+let a = dict[0];
+let b = dict[1];
+let c = dict[2];
+let d = dict[3];
+let e = dict[4];
+let f = dict[5];
+let g = dict[6];
+let h = dict[7];
+let i = dict[8];
+let j = dict[9];
+let k = dict[10];
+let l = dict[11];
+let m = dict[12];
+let n = dict[13];
+let o = dict[14];
+let p = dict[15];
+let q = dict[16];
+let r1 = dict[17];
+let s = dict[18];
+let t = dict[19];
+let u = dict[20];
+let v = dict[21];
+let w = dict[22];
+let r = Math.trunc(Math.random() * dict.length)
+let palabraCorrecta = dict[r];
+function handleSubmit(e){
+console.log(document.querySelectorAll('form'));
+let adivinanza = e.target.word.value;
+console.log(dict.includes(adivinanza));
+if (adivinanza == palabraCorrecta) {
+  console.log('si era esa')
+}else {
+  console.log('te equivocaste rey')
+}
+}
