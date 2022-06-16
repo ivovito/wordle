4368862f4421dca159a241cd35941c077655f341
karelitz <karelitz>
2022-06-16 11:15:13 -0300

    agrego

--- main.js
+++ main.js
@@ -25,38 +25,53 @@ let dict = [
   'genio',
   'pibes'
 ];
-let a = dict[0];
-let b = dict[1];
-let c = dict[2];
-let d = dict[3];
-let e = dict[4];
-let f = dict[5];
-let g = dict[6];
-let h = dict[7];
-let i = dict[8];
-let j = dict[9];
-let k = dict[10];
-let l = dict[11];
-let m = dict[12];
-let n = dict[13];
-let o = dict[14];
-let p = dict[15];
-let q = dict[16];
-let r1 = dict[17];
-let s = dict[18];
-let t = dict[19];
-let u = dict[20];
-let v = dict[21];
-let w = dict[22];
+
 let r = Math.trunc(Math.random() * dict.length)
 let palabraCorrecta = dict[r];
+
+let perdiste = "perdiste";
+let ganaste = "ganaste";
+let intentos = 0;
+console.log(palabraCorrecta);
 function handleSubmit(e){
-console.log(document.querySelectorAll('form'));
+
+e.preventDefault();
+document.querySelector('input');
+
 let adivinanza = e.target.word.value;
 console.log(dict.includes(adivinanza));
 if (adivinanza == palabraCorrecta) {
   console.log('si era esa')
 }else {
-  console.log('te equivocaste rey')
+  console.log('Nice try')
 }
+
+
+let i = 0;
+for (let c of adivinanza) {
+  console.log(c);
+  if (c == palabraCorrecta[i]) console.log("+");
+  else console.log("-");
+  i++;
+}
+
+
+
+if (intentos < 6) {
+  intentos++;
+  console.log(intentos);
+}else if (palabraCorrecta == adivinanza) {
+  console.log("ganaste")
+  // while (palabraCorrecta == adivinanza) {
+  //   console.log(palabraCorrecta);
+  //   handleSubmit;
+  // }
+} else {
+  console.log("perdiste");
+  // while (intentos >6 ) {
+  //   console.log(palabraCorrecta);
+  //   handleSubmit;
+  // }
+}
+
 }
