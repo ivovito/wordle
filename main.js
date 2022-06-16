f3dada3c827c563e28ef2c8f07a4eb8b7044125e
ivovito <ivokarelitz@gmail.com>
2022-06-16 12:53:12 -0300

    agregando

--- main.js
+++ main.js
@@ -29,49 +29,50 @@ let dict = [
 let r = Math.trunc(Math.random() * dict.length)
 let palabraCorrecta = dict[r];
 
-let perdiste = "perdiste";
-let ganaste = "ganaste";
+// let perdiste = "perdiste";
+// let ganaste = "ganaste";
 let intentos = 0;
 console.log(palabraCorrecta);
-function handleSubmit(e){
-
-e.preventDefault();
-document.querySelector('input');
-
-let adivinanza = e.target.word.value;
-console.log(dict.includes(adivinanza));
-if (adivinanza == palabraCorrecta) {
-  console.log('si era esa')
-}else {
-  console.log('Nice try')
-}
 
 
-let i = 0;
-for (let c of adivinanza) {
-  console.log(c);
-  if (c == palabraCorrecta[i]) console.log("+");
-  else console.log("-");
-  i++;
-}
+function handleSubmit(e) {
+  e.preventDefault();
+  // document.querySelector('input');
+  let adivinanza = e.target.word.value;
+  console.log(dict.includes(adivinanza)); // DEBUG
+  if (adivinanza == palabraCorrecta) {
+    console.log('si era esa')
+  } else {
+    console.log('Nice try')
+  }
 
 
+  let i = 0;
+  for (let c of adivinanza) {
+    console.log(c);
+    if (c == palabraCorrecta[i]) console.log("+");
+    else                         console.log("-");
+    i++;
+  }
 
-if (intentos < 6) {
-  intentos++;
-  console.log(intentos);
-}else if (palabraCorrecta == adivinanza) {
+  if (intentos < 6 && dict.includes(adivinanza)) {
+    intentos++;
+    console.log(intentos);
+  }
+  if (palabraCorrecta == adivinanza) {
   console.log("ganaste")
   // while (palabraCorrecta == adivinanza) {
   //   console.log(palabraCorrecta);
-  //   handleSubmit;
-  // }
-} else {
-  console.log("perdiste");
-  // while (intentos >6 ) {
-  //   console.log(palabraCorrecta);
-  //   handleSubmit;
-  // }
-}
+  //   // handleSubmit;
+  } else if (intentos == 6) {
+    console.log("perdiste");
+  }
 
+  if (intentos == 6 || palabraCorrecta == adivinanza) {
+    r = Math.trunc(Math.random() * dict.length);
+    palabraCorrecta = dict[r];
+    console.log(palabraCorrecta);
+    return;
+  }
+let div = document.createElement('div')
 }
